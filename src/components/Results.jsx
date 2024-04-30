3638c569fedbe05a605b7e5bafdbb900538323aa
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-05-01 00:08:31 +0200

    [FEAT] Getting repository information

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,35 +1,56 @@
 import React, {useEffect, useState} from 'react';
 import ResultCard from "./ResultCard";
+import { Octokit, App } from "octokit";
 
 const Results = (props) => {
     const [contributorPairs, setContributorPairs] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const findTopContributingPairs = () => {
-        setContributorPairs([
-            {
-                "developer1" : "Rogan003",
-                "developer2" : "TacTac",
-                "contributionsToTheSameFilesAndRepositories" : 20
-            },
-            {
-                "developer1" : "dev1",
-                "developer2" : "dev2",
-                "contributionsToTheSameFilesAndRepositories" : 15
-            },
-            {
-                "developer1" : "dev1",
-                "developer2" : "dev3",
-                "contributionsToTheSameFilesAndRepositories" : 12
-            }
-        ]);
+    const octokit = new Octokit();
 
-        if (props.linkToRepository === "hey")
+    const parseGitHubUrl = (url) => {
+        // example: https://github.com/Rogan003/TravelTheWorld (doesn't work with big R
+        const regex = /github\.com\/([^\/]+)\/([^\/]+)/;
+        const match = url.match(regex);
+        if (match) {
+            return { owner: match[1], repo: match[2] };
+        } else {
+            return { owner: null, repo: null };
+        }
+    }
+
+    const getRepository = () => {
+        const { owner, repo } = parseGitHubUrl(props.linkToRepository);
+
+        if (owner === null) return null;
+
+        octokit.request('GET /repos/{owner}/{repo}', {
+            owner: owner,
+            repo: repo
+        }).then(response => {
+            // Log the repository information
+            console.log("Repository Information:");
+            console.log("Name:", response.data.name);
+            console.log("Description:", response.data.description);
+            console.log("URL:", response.data.html_url);
+            return response.data;
+        }).catch(error => {
+            console.error("Error loading repository:", error);
+            return null;
+        });
+    }
+
+    const findTopContributingPairs = () => {
+        const repository = getRepository();
+
+        if(repository === null)
         {
             setContributorPairs([]);
-            setErrorMessage("Repository not found!")
+            setErrorMessage("Repository not found!");
+            return;
         }
 
+        setErrorMessage("");
         // other possible errors:
         // No commits found at the repository + props.linkToRepository!
         // Less than two developers contributed to the repository!
@@ -37,6 +58,12 @@ const Results = (props) => {
 
     useEffect(() => {
         findTopContributingPairs();
+
+        if(props.linkToRepository === "")
+        {
+            setErrorMessage("");
+        }
+
     }, [props.linkToRepository]);
 
     return (
